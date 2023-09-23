2d5ed373b2edc277df84d36d22205aeed7c05203
Dominic8999 <dominic3331@fedora.email>
2023-09-23 16:36:03 +0200

    proper code
    improved js code

--- index.js
+++ index.js
@@ -2,26 +2,24 @@ const quote = document.getElementById('quote')
 const author = document.getElementById('author')
 const btn = document.getElementById('btn-el')
 
-let JSON_quotes = 
-`[
+const quotes = [
     {
-        "text": "The only way to do great work is to love what you do.",
-        "author": "Steve Jobs"
+        text: "The only way to do great work is to love what you do.",
+        author: "Steve Jobs"
     },
     {
-        "text": "In three words I can sum up everything I've learned about life: it goes on.",
-        "author": "Robert Frost"
+        text: "In three words I can sum up everything I've learned about life: it goes on.",
+        author: "Robert Frost"
     },
     {
-        "text": "The best time to plant a tree was 20 years ago. The second best time is now.",
-        "author": "Chinese Proverb"
+        text: "The best time to plant a tree was 20 years ago. The second best time is now.",
+        author: "Chinese Proverb"
     }
-]`
-btn.addEventListener("click", () =>{
-    let quoteGeneration = () => {
-        let random = Math.floor(Math.random() * 3) + 1
-        quote.textContent += `${this.text}`
-        author.textContent += `${this.author}`
-    }
-    return quoteGeneration
+]
+
+btn.addEventListener("click",  () => {
+    const random_quote = Math.floor(Math.random() * quotes.length)
+    const generated_quote = quotes[random_quote]
+    quote.textContent = generated_quote.text
+    author.textContent = `${generated_quote.author}`
 })

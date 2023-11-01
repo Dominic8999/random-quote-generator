955dea9b3d3731f64d791cabee6f59c01a682762
Dominic8999 <dominic3331@fedora.email>
2023-11-01 23:19:46 +0100

    Previous button!
    Added previous button which shows the previous displayed quote.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const quote = document.querySelector('#quote');
 const author = document.querySelector('p:nth-of-type(2)'); // selects the second paragraph element
 const btn = document.querySelector('button');
+const prevButton = document.querySelector('#prev');
+const nextButton = document.querySelector('#next');
+
+let currentQuoteIndex = 0; // Initialize the index
 
 async function fetchQuotes() {
     try {
@@ -8,18 +12,34 @@ async function fetchQuotes() {
         const data = await response.json();
         return data.quotes;
     } catch (error) {
-        console.error('The quotes are unavailiable due to errror:', error);
+        console.error('The quotes are unavailable due to an error:', error);
     }
+}
+
+async function displayQuote(quotes, index) {
+    const generatedQuote = quotes[index];
+    quote.textContent = generatedQuote.text;
+    author.textContent = generatedQuote.author;
 }
 
 async function randomQuote() {
     const quotes = await fetchQuotes();
-    const randomQuoteIndex = Math.floor(Math.random() * quotes.length);
-    const generatedQuote = quotes[randomQuoteIndex];
-    quote.textContent = generatedQuote.text;
-    author.textContent = generatedQuote.author;
+    currentQuoteIndex = Math.floor(Math.random() * quotes.length);
+    displayQuote(quotes, currentQuoteIndex);
 }
 
 randomQuote();
 
 btn.addEventListener("click", randomQuote);
+
+prevButton.addEventListener("click", async () => {
+    const quotes = await fetchQuotes();
+    currentQuoteIndex = (currentQuoteIndex - 1 + quotes.length) % quotes.length;
+    displayQuote(quotes, currentQuoteIndex);
+});
+
+nextButton.addEventListener("click", async () => {
+    const quotes = await fetchQuotes();
+    currentQuoteIndex = (currentQuoteIndex + 1) % quotes.length;
+    displayQuote(quotes, currentQuoteIndex);
+});
